ad6765ad7df1d1e5a50930028679185be3d2f1fd
Andrea Polanco <alpm9615@gmail.com>
2022-06-12 22:29:48 -0400

    Added config file ;D

--- src/main/java/com/shop/apotheke/automated_framework/utils/ConfigurationManager.java
+++ src/main/java/com/shop/apotheke/automated_framework/utils/ConfigurationManager.java
@@ -2,18 +2,14 @@ package com.shop.apotheke.automated_framework.utils;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class ConfigurationManager {
-
+    public static ResourceBundle resourceBundle;
+    private static volatile ConfigurationManager manager;
     private ConfigurationManager() throws IOException {
-        PROPERTIES.load(ConfigurationManager.class.getResourceAsStream("default-config.properties"));
-        PROPERTIES.load(ConfigurationManager.class.getResourceAsStream("project-specific-config.properties"));
+        resourceBundle = ResourceBundle.getBundle("config");
     }
-
-    private static ConfigurationManager manager;
-
-    private static final Properties PROPERTIES = new Properties();
-
     public static ConfigurationManager getInstance() {
 
         if (manager == null) {
@@ -30,7 +26,7 @@ public class ConfigurationManager {
         return manager;
     }
 
-    public String getProperty(String name) {
-        return System.getProperty(name, PROPERTIES.getProperty(name));
+    public  String getKey(String key){
+        return resourceBundle.getString(key);
     }
 }
